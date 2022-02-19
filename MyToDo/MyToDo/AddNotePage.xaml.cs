e29814712034765a5896882a577291384cdc9602
Kal <info@kalacademy.org>
2022-02-19 12:28:53 -0800

    Initial commit

---
+++ MyToDo/MyToDo/AddNotePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MyToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyToDo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddNotePage : ContentPage
+    {
+        public AddNotePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if (string.IsNullOrEmpty(note.FileName))
+            {
+                //Create new file
+                note.FileName = Path.Combine(Environment.GetFolderPath(
+                    Environment.SpecialFolder.LocalApplicationData),
+                    $"{Path.GetRandomFileName()}.notes.txt");
+            }
+            File.WriteAllText(note.FileName, editor.Text);
+            await Navigation.PopModalAsync();
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MyToDo/MyToDo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MyToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyToDo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new AddNotePage { 
+                BindingContext = new Note()
+            });
+        }
+    }
+}
